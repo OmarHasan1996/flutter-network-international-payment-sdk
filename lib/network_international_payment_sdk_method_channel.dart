faf45331ddb3e0b63cc5f64cc8721e288d875e1b
CompassFiles <omar.suhail.hasan@gmail.com>
2025-12-03 12:50:54 +0400

    pass Google Pay Config

--- lib/network_international_payment_sdk_method_channel.dart
+++ lib/network_international_payment_sdk_method_channel.dart
@@ -24,6 +24,7 @@ class MethodChannelNetworkInternationalPaymentSdk extends NetworkInternationalPa
       'showOrderAmount': showOrderAmount,
       'showCancelAlert': showCancelAlert,
       'theme': theme,
+      'googlePayConfig': googlePayConfig,
     });
     return result;
   }
